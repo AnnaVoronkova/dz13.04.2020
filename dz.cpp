fbe7fcab4dee23694646f4c7ab12f06bdbfb4066
AnnaVoronkova <63228705+AnnaVoronkova@users.noreply.github.com>
2020-04-12 20:51:01 +0300

    Add files via upload

---
+++ dz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int dp[10000][50];
+    int n, m;
+    cin >> n >> m;
+    dp[1][1] = 1;
+    for (int i = 2; i <= n; i++){
+        for (int j = 2; j <= m; j++){
+                dp[i][j] = dp[i-1][j-2] + dp[i-2][j-1];
+        }
+    }
+    cout << dp[n][m];
+    return 0;
+}
